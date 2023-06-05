05f4644e1dfba287034b58d03ec0d93ae1afd19a
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-06-05 22:55:58 +0300

    CLI menu being added

--- waste-management.py
+++ waste-management.py
@@ -17,7 +17,7 @@ Base = declarative_base()
 class Bus(Base):
     __tablename__ = 'buses'
     id = Column(Integer, primary_key=True)
-    number = Column(String(20))
+    plate_number = Column(String(20))
     route_id = Column(Integer, ForeignKey('routes.id'))
     route = relationship("Route", back_populates="buses")
     driver_id = Column(Integer, ForeignKey('drivers.id'))
@@ -37,29 +37,92 @@ class Driver(Base):
     name = Column(String(100))
     buses = relationship("Bus", back_populates="driver")
 
+@click.command()
+def main():
+    choice = 0
+    while choice !=10:
+        print("*** Waste Disposal Management ***")
+        print("1) Add a bus")
+        print("2) Lookup a bus")
+        print("3) Display all buses")
+        print("4) Add a Route")
+        print("5) Lookup a Route")
+        print("6) Display all routes")
+        print("7) Add a driver")
+        print("8) Lookup a driver")
+        print("9) Display all drivers")
+        print("10) Quit")
+        choice = int(input())
 
-choice = 0
-while choice !=10:
-    print("*** Waste Disposal Management ***")
-    print("1) Add a bus")
-    print("2) Lookup a bus")
-    print("3) Display all buses")
-    print("4) Add a Route")
-    print("5) Lookup a Route")
-    print("6) Display all routes")
-    print("7) Add a driver")
-    print("8) Lookup a driver")
-    print("9) Display all drivers")
-    print("10) Quit")
-    choice = int(input())
+        if choice == 1:
+            add_bus()
+            print("Adding a bus")
+            break
 
-    if choice == 1:
-        print("Adding a bus")
+        elif choice == 2:
+            lookup_bus()
+            print("Looking for a bus")
+            break
 
-    elif choice == 2:
-        print("Looking for a bus")
+        elif choice == 3:
+            print("Displaying all buses")
+            break
 
+        elif choice == 4:
+            print("Adding a route")
+            break
+
+        elif choice == 5:
+            print("Looking for a route")
+            break
+
+        elif choice == 6:
+            print("Displaying all routes")
+            break
+
+        elif choice == 7:
+            print("Adding a driver")
+            break
+
+        elif choice == 8:
+            print("Looking for a driver")
+            break
+
+        elif choice == 9:
+            print("Displaying all drivers")
+            break
+
+        elif choice == 10:
+            print("Quitting Program")
+            break
+
+    print("Program Terminated")
+
+def add_bus():
+    plate_number = input("Enter the plate number of the bus: ")
+    route_id = int(input("Enter the route ID of the bus: "))
+    driver_id = int(input("Enter the driver ID of the bus: "))
+
+    bus = Bus(plate_number=plate_number, route_id=route_id, driver_id=driver_id)
+    session.add(bus)
+    session.commit()
+    print("Bus added successfully!")
+
+
+def lookup_bus():
+    plate_number = input("Enter the plate number of the bus to lookup: ")
+    bus = session.query(Bus).filter_by(plate_number=plate_number).first()
+
+    if bus:
+        print("Bus found!")
+        print("Plate Number:", bus.plate_number)
+        print("Route ID:", bus.route_id)
+        print("Driver ID:", bus.driver_id)
+        print("Datetime:", bus.datetime)
+    else:
+        print("Bus not found!")
 
 # Entry point
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    main()
