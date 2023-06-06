403926f72a1e8e013a6d3659680461bbe7886fe6
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-06-06 13:06:10 +0300

    add_bus method updated

--- main.py
+++ main.py
@@ -66,32 +66,40 @@ def main():
             break
 
         elif choice == 3:
+            update_bus_plate_number()
             break
 
         elif choice == 4:
+            delete_bus()
             break
 
         elif choice == 5:
             display_all_buses()
             break
 
-        elif choice == 6:
-            break
+        # elif choice == 6:
+        #     lookup_route()
+        #     break
 
-        elif choice == 7:
-            break
+        # elif choice == 7:
+        #     update_bus_route()
+        #     break
 
-        elif choice == 8:
-            break
+        # elif choice == 8:
+        #     display_all_routes()
+        #     break
 
-        elif choice == 9:
-            break
+        # elif choice == 9:
+        #     lookup_driver()
+        #     break
 
-        elif choice == 10:
-            break
+        # elif choice == 10:
+        #     update_bus_driver()
+        #     break
 
-        elif choice == 11:
-            break
+        # elif choice == 11:
+        #     display_all_drivers()
+        #     break
 
         elif choice == 12:
             print("Quitting Program")
@@ -104,25 +112,42 @@ def add_bus():
     plate_number = input("Enter the plate number of the bus: ")
     driver_name = input("Enter the name of the driver: ")
     route_path = input("Enter the route path (e.g., here - here): ")
+
+    # Check if the bus with the given plate number already exists
+    existing_bus = session.query(Bus).filter_by(plate_number=plate_number).first()
+    if existing_bus:
+        print("Error: Bus with the same plate number already exists!")
+        return
+
+    # Check if the route with the given path already exists
+    existing_route = session.query(Route).filter_by(path=route_path).first()
+    if existing_route:
+        print("Error: Route with the same path already exists!")
+        return
+
+    # Check if the driver is already assigned to three buses
     driver = session.query(Driver).filter_by(name=driver_name).first()
-    if driver is None:
+    if driver:
+        assigned_buses_count = session.query(Bus).filter_by(driver=driver).count()
+        if assigned_buses_count >= 3:
+            print("Error: The driver is already assigned to three buses!")
+            return
+    else:
         driver = Driver(name=driver_name)
         session.add(driver)
         session.commit()
 
-    route = session.query(Route).filter_by(path=route_path).first()
-    if route is None:
-        route = Route(path=route_path)
-        session.add(route)
-        session.commit()
+    route = Route(path=route_path)
+    session.add(route)
+    session.commit()
 
     now = datetime.utcnow()
     datetime_str = now.strftime("%Y-%m-%d %H:%M")
     bus = Bus(plate_number=plate_number, route=route, driver=driver, datetime=datetime_str)
     session.add(bus)
     session.commit()
-    print("Bus added successfully!")
 
+    print("Bus added successfully!")
 
 def lookup_bus():
     plate_number = input("Enter the plate number of the bus to lookup: ")
@@ -137,6 +162,41 @@ def lookup_bus():
     else:
         print("Bus not found!")
 
+
+def update_bus_plate_number():
+    plate_number = input("Enter the plate number of the bus to update: ")
+    bus = session.query(Bus).filter_by(plate_number=plate_number).first()
+
+    if bus:
+        new_plate_number = input("Enter the new plate number: ")
+        bus_with_new_plate = session.query(Bus).filter_by(plate_number=new_plate_number).first()
+
+        if bus_with_new_plate:
+            print("Error: Another bus already has the new plate number.")
+        else:
+            bus.plate_number = new_plate_number
+            session.commit()
+            print("Bus plate number updated successfully!")
+    else:
+        print("Error: Bus not found!")
+
+def delete_bus():
+    plate_number = input("Enter the plate number of the bus to delete: ")
+    bus = session.query(Bus).filter_by(plate_number=plate_number).first()
+
+    if bus:
+        driver = bus.driver
+        route = bus.route
+
+        session.delete(bus)
+        session.delete(driver)
+        session.delete(route)
+
+        session.commit()
+        print("Bus and associated driver and route deleted successfully!")
+    else:
+        print("Error: Bus not found!")
+
 def display_all_buses():
     buses = session.query(Bus).all()
     print("All Buses:")
@@ -148,6 +208,7 @@ def display_all_buses():
         formatted_datetime = datetime_obj.strftime("%Y-%m-%d %H:%M")
         print("Datetime:", formatted_datetime)
         print()
+
 
 # Entry point
 if __name__ == '__main__' or __file__ == 'main.py':
