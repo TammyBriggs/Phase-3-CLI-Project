010c8549ea81a2c75ea367547ce36c70aa2575bf
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-06-09 09:12:33 +0300

    Migration Environment updated

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from main import Base
+target_metadata = Base.metadata
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
---
+++ app/migrations/versions/26b810d53a96_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 26b810d53a96
+Revises: 
+Create Date: 2023-06-09 09:11:13.716239
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '26b810d53a96'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
